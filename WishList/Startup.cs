ac278983d71cdf7498e738e8e8396aa8a405c93b
dvegas13 <83663164+dvegas13@users.noreply.github.com>
2021-07-14 15:28:44 -0400

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WishList.Data;
 
 namespace WishList
 {
